9e8e6e5dc5482775c744e015f425c786b47315e3
JohnPaul <japam@secret6.com>
2020-12-26 23:25:43 +0800

    Implement Follow Cursor function

---
+++ HoloStage/Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class FollowMouse : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+    private Vector2 position = Vector2.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void MoveTarget()
+    {
+        if( MouseScreenCheck() )
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            position = Vector2.Lerp( transform.position, mousePosition, moveSpeed);
+            transform.position = position;
+        }
+    }
+
+    public bool MouseScreenCheck(){
+        #if UNITY_EDITOR
+        if (Input.mousePosition.x == 0 || Input.mousePosition.y == 0 || Input.mousePosition.x >= Handles.GetMainGameViewSize().x - 1 || Input.mousePosition.y >= Handles.GetMainGameViewSize().y - 1){
+            return false;
+        }
+        #else
+        if (Input.mousePosition.x == 0 || Input.mousePosition.y == 0 || Input.mousePosition.x >= Screen.width - 1 || Input.mousePosition.y >= Screen.height - 1) {
+            return false;
+        }
+        #endif
+        else {
+            return true;
+        }
+    }
+
+}
--- HoloStage/Assets/Scripts/PointConstrains.cs
+++ HoloStage/Assets/Scripts/PointConstrains.cs
@@ -4,16 +4,65 @@ using UnityEngine;
 
 public class PointConstrains : MonoBehaviour
 {
-    public BoxCollider2D[] m_bounds;
+    public Transform m_target;
+    public Collider2D[] m_bounds;
+
+    private Vector2 m_lastPos = Vector2.zero;
+    private Vector2 m_cp;
+    private Vector3 m_mousePosition;
+    public float m_moveSpeed = 0.1f;
+    private Vector2 m_position = Vector2.zero;
+    // private List<Vector2> m_closePoints = new List<Vector2>();
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if( m_target == null )
+        {
+            return;
+        }
+        m_mousePosition = Input.mousePosition;
+        m_mousePosition = Camera.main.ScreenToWorldPoint(m_mousePosition);
+        m_position = Vector2.Lerp( m_target.position, m_mousePosition, m_moveSpeed);
+        // m_target.position = m_position;
+
+        if( (Vector2)m_mousePosition == m_lastPos )
+        {
+            
+            return;
+        }
+        else
+        {
+            // m_closePoints.Clear();
+            m_lastPos = m_mousePosition;
+            m_cp = (Vector3)m_position + (Vector3.one * 100f);
+            int overlap = 0;
+
+            for( int i = 0 ; i < m_bounds.Length; i++ )
+            {
+                if (m_bounds[i].OverlapPoint(m_position) )
+                {
+                    overlap++;
+                }
+                else
+                {
+                    Vector2 t = m_bounds[i].ClosestPoint(m_position);
+                    m_cp =  Vector2.Distance( m_lastPos, t) < Vector2.Distance( m_lastPos, m_cp) ? t:m_cp;
+                }
+            }
+            if( overlap == 0 )
+            {
+                Debug.LogError("Not inside bounds");
+                m_target.position = m_cp;
+            }
+            else{
+                m_target.position = m_position;
+            }
+        }
     }
 }
