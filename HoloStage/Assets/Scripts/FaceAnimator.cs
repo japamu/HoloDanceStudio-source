a4b4bf36f1a1bbe02b467dd8114c1dfc8ba45ef3
JohnPaul <japam@secret6.com>
2020-12-27 18:04:39 +0800

    Implemented Basic Face Animator

---
+++ HoloStage/Assets/Scripts/FaceAnimator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceAnimator : MonoBehaviour
+{
+    public Animator m_animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetKeyDown(KeyCode.Alpha1) )
+        {
+            Debug.Log("Pressed");
+            m_animator.Play("a_eye_normal", 0 );
+        }
+        else if( Input.GetKeyDown(KeyCode.Alpha2) )
+        {
+            Debug.Log("Pressed");
+            m_animator.Play("a_eye_playful", 0 );
+        }
+        if( Input.GetKeyDown(KeyCode.Q) )
+        {
+            m_animator.Play("a_mouth_smile_large", 1 );
+        }
+        else if( Input.GetKeyDown(KeyCode.W) )
+        {
+            m_animator.Play("a_mouth_v", 1 );
+        }
+        else if( Input.GetKeyDown(KeyCode.E) )
+        {
+            m_animator.Play("a_mouth_a", 1 );
+        }
+
+    }
+}
