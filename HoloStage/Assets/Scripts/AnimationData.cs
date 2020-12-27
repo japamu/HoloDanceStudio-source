15031cf7ca3f25a440c660ab22fe8b8239352ac9
JohnPaul <japam@secret6.com>
2020-12-27 18:23:26 +0800

    Created AnimationData and Animation Library for configurable animation settings

---
+++ HoloStage/Assets/Scripts/AnimationData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AnimationData", menuName = "Holostage/Animation Data")]
+public class AnimationData : ScriptableObject
+{
+    public string m_name;
+    public Sprite m_image;
+
+}
---
+++ HoloStage/Assets/Scripts/AnimationLibrary.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AnimationLibrary", menuName = "Holostage/Animation Library")]
+public class AnimationLibrary : ScriptableObject
+{
+    public AnimationData[] eyeAnimation;
+    public AnimationData[] mouthAnimation;
+}
