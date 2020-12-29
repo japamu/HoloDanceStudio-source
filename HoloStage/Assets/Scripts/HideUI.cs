0327cba84a8b41d6d58861afbea91edb9dbfc6be
JohnPaul <japam@secret6.com>
2020-12-29 18:53:14 +0800

    Implemented Toggle Interactability for hiding canvasses Renamed UI_Canvas to Hotkey Canvas

--- HoloStage/Assets/Scripts/HideUI.cs
+++ HoloStage/Assets/Scripts/HideUI.cs
@@ -5,23 +5,16 @@ using UnityEngine;
 public class HideUI : MonoBehaviour
 {
     public CanvasGroup[] m_hideCanvasGroup;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void ToggleHideUI( bool p_toggle )
     {
         for( int i = 0 ; i < m_hideCanvasGroup.Length ; i++ )
         {
             m_hideCanvasGroup[i].alpha = p_toggle?1:0;
+            if( m_hideCanvasGroup[i].GetComponent<ToggleInteractability>() != null )
+            {
+                m_hideCanvasGroup[i].GetComponent<ToggleInteractability>().Toggle( p_toggle );
+            }
         }
     }
 }
---
+++ HoloStage/Assets/Scripts/ToggleInteractability.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleInteractability : MonoBehaviour
+{
+    private CanvasGroup m_canvasGrp;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_canvasGrp = GetComponent<CanvasGroup>();   
+    }
+    public void Toggle( bool p_interactable )
+    {
+        if( m_canvasGrp != null )
+        {
+            m_canvasGrp.interactable = p_interactable;
+        }
+    }
+}
