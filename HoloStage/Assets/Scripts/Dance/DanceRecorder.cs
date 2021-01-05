be8436ca1193b2575ce456c6377ea7bcbc845342
JohnPaul <japam@secret6.com>
2021-01-05 10:12:49 +0800

    Added Record Button Effect Added DanceRecorderScript Added MonoInstance

---
+++ HoloStage/Assets/Scripts/Dance/DanceRecorder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceRecorder : MonoInstance<DanceRecorder>
+{
+    public RecorderButton[] m_recordButton;
+    private bool b_isRecording;
+    private DanceData m_danceData;
+
+    void Start()
+    {
+        b_isRecording = false;
+    }
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    public void OnRecordButtonPressed()
+    {
+        b_isRecording = !b_isRecording;
+        for( int i = 0 ; i < m_recordButton.Length ; i++ )
+        {
+            m_recordButton[i].SetIconToRecording( b_isRecording );
+        }
+    }
+
+    public void RecordAnimation( AnimationData p_data )
+    {
+        
+    }
+
+    public void RecordAnimation( SavedFollowData p_data )
+    {
+        
+    }
+}
---
+++ HoloStage/Assets/Scripts/Dance/RecorderButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecorderButton : MonoBehaviour
+{
+    public GameObject m_recordEffect;
+    public void SetIconToRecording( bool p_isRecording )
+    {
+        m_recordEffect.SetActive( p_isRecording );
+    }
+}
