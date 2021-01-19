ba58e33adc72e2e4ec5823764ec2e48cf9db404f
JohnPaul <japam@secret6.com>
2021-01-19 11:49:45 +0800

    Polished Record Help Screen Added Fullscreen Button

---
+++ HoloStage/Assets/Scripts/FullscreenManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullscreenManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ( Utils.IsMobile() )
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnButtonPress()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+}
