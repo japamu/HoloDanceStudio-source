f0d1d53bba1d408dc335393c05934542266fb7ec
JohnPaul <japam@secret6.com>
2021-01-20 23:58:00 +0800

    Added another help screen for general navigation Added Platform Dependent Text for easier switching Added Mobile Restrictions script for specific buttons

---
+++ HoloStage/Assets/Scripts/MobileRestrictions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MobileRestrictions : MonoBehaviour
+{
+    public GameObject[] m_disabled;
+    public Button[] m_restricted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if( Utils.IsMobile() )
+        {
+            for( int i  = 0 ; i < m_restricted.Length ; i++ )
+            {
+                m_restricted[i].interactable = false;
+            }
+            for( int i  = 0 ; i < m_disabled.Length ; i++ )
+            {
+                m_disabled[i].SetActive( false );
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
