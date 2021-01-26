8c9a4981e5071b4dbde610d9b9fcba96c2426c98
JohnPaul <japam@secret6.com>
2021-01-26 23:09:40 +0800

    Added AR button to main screen Added Platform Dependent Image for easier UI changes

--- HoloStage/Assets/Scripts/MobileRestrictions.cs
+++ HoloStage/Assets/Scripts/MobileRestrictions.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class MobileRestrictions : MonoBehaviour
 {
+    public GameObject[] m_enabled;
     public GameObject[] m_disabled;
     public Button[] m_restricted;
     // Start is called before the first frame update
@@ -20,6 +21,21 @@ public class MobileRestrictions : MonoBehaviour
             for( int i  = 0 ; i < m_disabled.Length ; i++ )
             {
                 m_disabled[i].SetActive( false );
+            }
+            for( int i  = 0 ; i < m_enabled.Length ; i++ )
+            {
+                m_enabled[i].SetActive( true );
+            }
+        }
+        else
+        {
+            for( int i  = 0 ; i < m_disabled.Length ; i++ )
+            {
+                m_disabled[i].SetActive( true );
+            }
+            for( int i  = 0 ; i < m_enabled.Length ; i++ )
+            {
+                m_enabled[i].SetActive( false );
             }
         }
     }
---
+++ HoloStage/Assets/Scripts/PlatformDependentImage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlatformDependentImage : MonoBehaviour
+{
+    public Image m_image;
+    public Sprite sprite_pc;
+    public Sprite sprite_android;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if( Utils.IsMobile() )
+        {
+            m_image.sprite = sprite_android;
+        }
+        else
+        {
+            m_image.sprite = sprite_pc;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
