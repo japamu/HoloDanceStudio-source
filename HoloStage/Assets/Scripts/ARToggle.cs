69d63101946dc095e5c05292b03fec5c8b10ac49
JohnPaul <japam@secret6.com>
2021-01-27 00:17:26 +0800

    Added AR toggle Button to AR scene Added adapting message for ARToggle Script

--- HoloStage/Assets/Scripts/ARToggle.cs
+++ HoloStage/Assets/Scripts/ARToggle.cs
@@ -1,17 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ARToggle : MonoBehaviour
 {
+    public Text m_confirmationScreenMessage;
+    public Text m_confirmationButton;
     private bool isOnARScene;
     private static string AR_SCENE_NAME = "Stage_AR-X";
     private static string NONAR_SCENE_NAME = "Stage_0";
+    private static string START_AR_MODE = "Start AR Mode?";
+    private static string EXIT_AR_MODE = "Exit AR Mode?";
+    private static string MSG_START = "Start";
+    private static string MSG_EXIT = "Exit";
     // Start is called before the first frame update
     void Start()
     {
         isOnARScene = (SceneManager.GetActiveScene().name == AR_SCENE_NAME);
+        if( isOnARScene )
+        {
+            //Start AR Mode?
+            m_confirmationScreenMessage.text = EXIT_AR_MODE;
+            m_confirmationButton.text = MSG_EXIT;
+        }
+        else
+        {
+            m_confirmationScreenMessage.text = START_AR_MODE;
+            m_confirmationButton.text = MSG_START;
+        }
     }
 
     // Update is called once per frame
