84633ea53ca033357502013b82adddfe4c42a719
JohnPaul <japam@secret6.com>
2021-01-30 23:46:32 +0800

    Fixed Help Screen for mobile devices

---
+++ HoloStage/Assets/Scripts/PlatformDependentAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDependentAnimator : MonoBehaviour
+{
+    public Animator m_animator;
+    public string animstate_pc;
+    public string animstate_android;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if( Utils.IsMobile() )
+        {
+            m_animator.Play( animstate_android );
+        }
+        else
+        {
+            m_animator.Play( animstate_pc );
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
