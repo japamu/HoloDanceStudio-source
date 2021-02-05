daac9c16af530075c44e27c0c86e03f436a32f12
JohnPaul <japam@secret6.com>
2021-02-05 23:18:09 +0800

    Added Splashscreen

---
+++ HoloStage/Assets/_splashscreen/SceneChangeDelay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeDelay : MonoBehaviour
+{
+    public float m_delay;
+    public string m_sceneDestination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke( nameof(SceneChange), m_delay );
+    }
+
+    void SceneChange()
+    {
+        SceneManager.LoadScene( m_sceneDestination, LoadSceneMode.Single);
+    }
+}
