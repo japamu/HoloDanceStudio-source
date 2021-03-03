578708b3f494a8c96ce58f9f2a01c5de65a2acc8
JohnPaul <japam@secret6.com>
2021-03-03 10:38:05 +0800

    Initial commit for enabling app for nonARCore devices to do: add loading icon for downloading

--- HoloStage/Assets/Scripts/ARAvailability.cs
+++ HoloStage/Assets/Scripts/ARAvailability.cs
@@ -8,7 +8,9 @@ using UnityEngine.XR.ARCore;
 #endif
 public class ARAvailability : MonoBehaviour
 {
-    public Button m_button;
+    public Button[] m_button;
+    public GameObject m_ARConfirmationScreen;
+
 #if UNITY_ANDROID
 
     private void OnEnable()
@@ -16,6 +18,42 @@ public class ARAvailability : MonoBehaviour
         StartCoroutine(ARSession.CheckAvailability());
         StartCoroutine(AllowARScene());
         // m_button.interactable = false;
+    }
+
+    private void SetButtonState( bool p_state, bool p_show=false )
+    {
+        for( int i = 0 ; i < m_button.Length ; i++ )
+        {
+            m_button[i].interactable = p_state;
+            m_button[i].gameObject.SetActive( p_show? true: p_state);
+        }
+    }
+
+    public void ShowConfirmationScreen()
+    {
+        Debug.Log("Show AR Confirmation Screen");
+        switch( ARSession.state )
+        {
+            case ARSessionState.Unsupported:
+                Debug.Log("AR Unsupported");
+                SetButtonState(false);
+            break;
+            case ARSessionState.NeedsInstall:
+                Debug.Log("AR Needs Installing");
+                //add coroutine for rotating button
+                ARSession.Install();
+            break;
+            case ARSessionState.Installing:
+                Debug.Log("AR Installing");
+            break;
+            case ARSessionState.Ready:
+            case ARSessionState.SessionInitializing:
+            case ARSessionState.SessionTracking:
+                Debug.Log("AR Ready");
+                m_ARConfirmationScreen.SetActive(true);
+            break;
+        }
+        
     }
  
     IEnumerator AllowARScene()
@@ -26,21 +64,19 @@ public class ARAvailability : MonoBehaviour
                 ARSession.state == ARSessionState.None)
             {
                 Debug.Log("Waiting...");
-                m_button.interactable = false;
+                SetButtonState(false, true);
                 yield return null;
             }
             if (ARSession.state == ARSessionState.Unsupported)
             {
                 Debug.Log("AR unsupported");
-                gameObject.SetActive( false );
-                m_button.interactable = false;
+                SetButtonState(false);
                 yield break;
             }
             if (ARSession.state > ARSessionState.CheckingAvailability)
             {
                 Debug.Log("AR supported");
-                gameObject.SetActive( true );
-                m_button.interactable = true;
+                SetButtonState(true);
                 yield break;
             }
         }      
